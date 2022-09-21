3d899e06f7f35e541d6913463802eb0f37ea1e39
mrdeecy <equarshie258@gmail.com>
2022-09-20 20:20:09 -0700

    Add: Task 2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _memset - sets memory to array
+ * _strcat - sets memory to array
  * @s: array to set
  * @b: value to set it as
  * @n: n amount of times
  * Return: char value of s
  */
-char *_memset(char *s, char b, unsigned int n)
+char *_strcat(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+
+**
+ * _strcat - copies from source to dest values in array
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
+ */
+char *_strcat(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
